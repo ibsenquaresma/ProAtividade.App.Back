dc41804c094f78254b043f922aa8ecd63b953e72
Ibsen Quaresma <ibsen@quaresma.me>
2023-11-06 16:46:55 -0500

    Add project files.

---
+++ ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Model;
+
+namespace ProAtividade.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtividadeController : ControllerBase
+    {
+        public IEnumerable<Atividade> Atividades = new List<Atividade>()
+            {
+                new Atividade(1),
+                new Atividade(2),
+                new Atividade(3)
+            };
+
+        public AtividadeController() { }
+        
+        [HttpGet]
+        public IEnumerable<Atividade> Get()
+        {
+            return Atividades;
+        }
+
+        [HttpGet("{id}")]
+        public Atividade Get(int id)
+        {
+            return Atividades.FirstOrDefault(ativ => ativ.Id == id);
+        }
+
+        [HttpPost]
+        public IEnumerable<Atividade> Post(Atividade atividade)
+        {
+            return Atividades.Append<Atividade>(atividade);
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Meu primeiro put";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Meu primeiro put com parametro {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Meu primeiro delete {id}";
+        }
+    }
+}
---
+++ ProAtividade.API/Model/Atividade.cs
@@ -0,0 +1,22 @@
+﻿namespace ProAtividade.API.Model
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Prioridade { get; set; }
+
+
+        public Atividade() { }
+
+        public Atividade(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
